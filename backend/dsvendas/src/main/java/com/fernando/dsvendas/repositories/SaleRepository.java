4612371d3899404e4ca27132322310be7d342ab4
Fernando Salvetti <fernandogsalvetti@gmail.com>
2021-05-07 00:30:10 -0300

    Pagination

---
+++ backend/dsvendas/src/main/java/com/fernando/dsvendas/repositories/SaleRepository.java
@@ -0,0 +1,10 @@
+package com.fernando.dsvendas.repositories;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.fernando.dsvendas.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long>{
+
+}
